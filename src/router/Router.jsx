afdd1a391ce22bb93390b5811ead3c4cc04c2364
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2025-02-07 11:18:46 +0545

    make layout page and change in router, main.jsx and vars input in eslint.config.js file

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
 
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>
+        element: <Main/>,
     },
 ]);
 
