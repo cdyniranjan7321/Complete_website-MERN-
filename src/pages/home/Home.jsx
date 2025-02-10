2e75d054b9ac41b184d520bf07f77cc2972aa4d0
Niranjan Chaudhary <cdyniranjan7321@gmail.com>
2025-02-10 16:38:56 +0545

    make change in components and pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Banner from '../../components/Banner'
 import Categories from './Categories';
-import SpecialDishes from './SpecialDishes';
+import SpecialServices from './SpecialServices';
+import OurServices from './OurServices';
+import Testimonials from './Testimonials';
+import OurExpertises from './OurExpertise';
 
 const Home = () => {
   return (
     <div>
     <Banner/>
     <Categories/>
-    <SpecialDishes/>
+    <SpecialServices/>
+    <OurServices/>
+    <OurExpertises/>
+    <Testimonials/>
     </div>
   )
 }
