1ca79de4c3064bf8631a5ba6d6fe2f099b94d84d
damber7 <dambert999@gmail.com>
2025-02-12 14:48:30 +0545

    Changes in expertise section

--- src/pages/home/Appointments.jsx
+++ src/pages/home/Appointments.jsx
@@ -5,21 +5,21 @@ const Appointments = () => {
   return (
     <div className="section-container">
       <div className="flex flex-col md:flex-row items-center justify-between gap-16">
-        <div className="relative md:w-1/2">
+        <div className="relative md:w-1/2 top-[-190px] left-[-80px]">
           {/* Image */}
           <img
             src={appointmentImg}
             alt="Appointment"
-            className="w-[492px] h-[731px] object-cover"
+            className="w-[600px] h-[1000px] object-cover"
           />
 
           {/* Overlayed Sections */}
-          <div className="absolute top-[57.3%] left-3/4 transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-[7px_12px_43px_0_#00000023] rounded-[30px] w-[260px] h-[112px] text-center space-y-2 text-green cursor-pointer hover:border hover:border-indigo-600 transition-all duration-200 flex flex-col justify-center">
+          <div className="absolute top-[70.3%] left-[85%] transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-[7px_12px_43px_0_#00000023] rounded-[30px] w-[260px] h-[112px] text-center space-y-2 text-green cursor-pointer hover:border hover:border-indigo-600 transition-all duration-200 flex flex-col justify-center">
             <h5 className="text-[20px] font-bold">SUN - FRI</h5>
             <p className="text-[16px] text-[#90BD95] font-semibold">8AM - 10PM</p>
           </div>
 
-          <div className="absolute top-[76.5%] left-3/4 transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-[7px_12px_43px_0_#00000023] rounded-[30px] w-[260px] h-[112px] text-center space-y-2 text-green cursor-pointer hover:border hover:border-indigo-600 transition-all duration-200 flex flex-col justify-center">
+          <div className="absolute top-[84%] left-[85%] transform -translate-x-1/2 -translate-y-1/2 bg-white shadow-[7px_12px_43px_0_#00000023] rounded-[30px] w-[260px] h-[112px] text-center space-y-2 text-green cursor-pointer hover:border hover:border-indigo-600 transition-all duration-200 flex flex-col justify-center">
             <h5 className="text-[20px] font-bold">SAT</h5>
             <p className="text-[16px] text-[#90BD95] font-semibold">Closed</p>
           </div>
